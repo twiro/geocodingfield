2e9085028d59dacd4881aa715d875650e46bad97
Jonas Coch <jonas@klaftertief.de>
2012-05-12 23:14:10 +0200

    Updated processRawFieldData() signature

--- fields/field.geocoding.php
+++ fields/field.geocoding.php
@@ -177,7 +177,7 @@
 			return self::__OK__;
 		}
 		
-		public function processRawFieldData($data, &$status, $simulate = false, $entry_id = null) {
+		public function processRawFieldData($data, &$status, &$message = null, $simulate = false, $entry_id = null) {
 			$status = self::__OK__;
 			
 			return array(
